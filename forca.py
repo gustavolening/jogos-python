f14964f2cdaadb2e9d33c14efbfb71217211c022
gustavolening <gustavo.-godoy@hotmail.com>
2023-04-17 22:33:59 -0300

    guardando as letras acertadas

--- forca.py
+++ forca.py
@@ -4,19 +4,23 @@ def jogar():
     print("*********************************")
 
     palavra_secreta = "banana"
+    letras_acertadas = ['_','_','_','_','_','_']
 
     enforcou = False
     acertou = False
+
+    print("Fruta com {} letras!".format(str(letras_acertadas.count('_'))), "\n", letras_acertadas)
 
     #Enquanto não enforcou E não acertou
     while(not enforcou and not acertou):
 
         chute = input("Qual a letra?").strip() ##Strip serve para tirar os espaços antes e depois da palavra.
 
+
         index = 0
         for letra in palavra_secreta:
             if(chute.upper() == letra.upper()):
-                print("Encontrei a letra {} na posição {}".format(chute, index))
+                letras_acertadas[index] = letra
             index = index + 1
 
         print("Jogando.........")
