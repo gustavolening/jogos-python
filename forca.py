563c572078f27a32ed3f5fef2187053ade70baba
gustavolening <gustavo.-godoy@hotmail.com>
2023-04-17 21:44:21 -0300

    Conhecendo e trabalhando com listas

--- forca.py
+++ forca.py
@@ -11,11 +11,11 @@ def jogar():
     #Enquanto não enforcou E não acertou
     while(not enforcou and not acertou):
 
-        chute = input("Qual a letra?")
+        chute = input("Qual a letra?").strip() ##Strip serve para tirar os espaços antes e depois da palavra.
 
         index = 0
         for letra in palavra_secreta:
-            if(chute == letra):
+            if(chute.upper() == letra.upper()):
                 print("Encontrei a letra {} na posição {}".format(chute, index))
             index = index + 1
 
